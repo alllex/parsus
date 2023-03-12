0b2d4cebe257d264128bcd2c93143ecef49e018e
Alex Semin <alllexsm@gmail.com>
2023-03-12 12:53:49 +0100

    Use Dokka for javadoc.jar generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     buildsrc.conventions.`kotlin-multiplatform`
     buildsrc.conventions.`maven-publishing`
     id("io.github.gradle-nexus.publish-plugin") version "1.2.0"
+    id("org.jetbrains.dokka")
 }
 
 group = "me.alllex.parsus"
@@ -20,6 +21,26 @@ kotlin {
 
 repositories {
     mavenCentral()
+}
+
+val javadocJar: TaskProvider<Jar> by tasks.registering(Jar::class) {
+    description = "Produce javadoc with Dokka HTML inside"
+    dependsOn(tasks.dokkaHtml)
+    from(tasks.dokkaHtml)
+    archiveClassifier.set("javadoc")
+}
+
+// Without this there is a Gradle error (notice mismatch between publish task and sign names):
+// > Reason: Task ':publishIosArm64PublicationToMavenLocal' uses this output of task ':signIosX64Publication' without declaring an explicit or implicit dependency.
+tasks.withType<AbstractPublishToMaven>().configureEach {
+    mustRunAfter(tasks.withType<Sign>())
+}
+
+// Maven Central publication requires a javadoc jar
+publishing {
+    publications.withType<MavenPublication>().configureEach {
+        artifact(javadocJar)
+    }
 }
 
 nexusPublishing {
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,4 +6,5 @@ dependencies {
     val kotlinVer = "1.8.10"
     implementation(platform(kotlin("bom", kotlinVer)))
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVer")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:$kotlinVer")
 }
