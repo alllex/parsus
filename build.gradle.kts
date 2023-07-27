90eb5b91ad92d4f2b0e4347dd17063097c3c6281
Alex Semin <alllexsm@gmail.com>
2023-07-27 23:31:01 +0200

    Check quick ref in readme

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,3 +56,22 @@ tasks.register<GenerateQuickReferenceMarkdown>("generateQuickRef") {
     kotlinTestSource = project.layout.projectDirectory.file("src/commonTest/kotlin/me/alllex/parsus/ReadmeTests.kt")
     markdownOutput = project.layout.buildDirectory.file("quickref.md")
 }
+
+tasks.register("checkQuickRefInReadme") {
+    dependsOn("generateQuickRef")
+    val generatedQuickRef = project.layout.buildDirectory.file("quickref.md")
+    val readme = project.layout.projectDirectory.file("README.md")
+    inputs.file(generatedQuickRef)
+    inputs.file(readme)
+    doLast {
+        val generatedQuickRefString = generatedQuickRef.get().asFile.readText()
+        val readmeAsString = readme.asFile.readText()
+        check(readmeAsString.contains(generatedQuickRefString)) {
+            "Generated quick reference is not present in README.md"
+        }
+    }
+}
+
+tasks.named("check") {
+    dependsOn("checkQuickRefInReadme")
+}
