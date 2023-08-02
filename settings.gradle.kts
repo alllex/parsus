903794eb1e1a456ca958f030a172c7f47e6e8ee6
Alex Semin <alllexsm@gmail.com>
2023-08-02 22:56:52 +0200

    Apply GE plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,17 @@
 plugins {
+    id("com.gradle.enterprise") version "3.14.1"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.6.0"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        if (settings.providers.environmentVariable("CI").orNull != null) {
+            publishAlways()
+            tag("CI")
+        }
+    }
 }
 
 rootProject.name = "parsus"
