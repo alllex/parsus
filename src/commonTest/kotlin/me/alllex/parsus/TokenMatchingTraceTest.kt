3368259b5b5eebc84895e6acc1e7ef495bcfad19
Alex Semin <alllexsm@gmail.com>
2023-10-05 22:33:43 +0200

    Fix TokenMatchingTraceTest

--- src/commonTest/kotlin/me/alllex/parsus/TokenMatchingTraceTest.kt
+++ src/commonTest/kotlin/me/alllex/parsus/TokenMatchingTraceTest.kt
@@ -27,44 +27,87 @@ class TokenMatchingTraceTest {
             val p by aOrB * cdOrEf map { (v1, v2) -> node(v1, v2) }
             override val root by oneOrMore(p) map { node(it) }
         }.run {
-            val input = "aefbcd"
+            val input = " a ef  b cd "
             val tracedResult = parseTracingTokenMatching(input)
             assertThat(tracedResult.result).isEqualTo(ParsedValue(
-                node(node(a.lex(0), ef.lex(1)), node(b.lex(3), cd.lex(4))))
+                node(node(a.lex(1), ef.lex(3)), node(b.lex(7), cd.lex(9))))
             )
             val formattedTrace = formatTokenMatchingTrace(tracedResult.trace)
             println(formattedTrace)
             assertThat("\n" + formattedTrace).isEqualTo(
                 """
-____________
-······aefbcd
-      ^ [0 - 0] a LiteralToken('a')
-____________
-·····aefbcd·
-      x [1] cd LiteralToken('cd')
-      x [1] ws RegexToken(ws [\s+] [ignored])
-____________
-·····aefbcd·
-      ^^ [1 - 2] ef LiteralToken('ef')
-____________
-···aefbcd···
-      x [3] a LiteralToken('a')
+__________________
+······␣a␣ef␣␣b␣cd␣
+      x [0] a LiteralToken('a')
+__________________
+······␣a␣ef␣␣b␣cd␣
+      ^ [0 - 0] ws RegexToken(ws [\s+] [ignored])
+__________________
+·····␣a␣ef␣␣b␣cd␣·
+      ^ [1 - 1] a LiteralToken('a')
+__________________
+····␣a␣ef␣␣b␣cd␣··
+      x [2] cd LiteralToken('cd')
+__________________
+····␣a␣ef␣␣b␣cd␣··
+      ^ [2 - 2] ws RegexToken(ws [\s+] [ignored])
+__________________
+···␣a␣ef␣␣b␣cd␣···
+      x [3] cd LiteralToken('cd')
       x [3] ws RegexToken(ws [\s+] [ignored])
-____________
-···aefbcd···
-      ^ [3 - 3] b LiteralToken('b')
-____________
-··aefbcd····
-      ^^ [4 - 5] cd LiteralToken('cd')
-____________
-…efbcd······
-      x [6] a LiteralToken('a')
-      x [6] ws RegexToken(ws [\s+] [ignored])
-      x [6] b LiteralToken('b')
-      x [6] ws RegexToken(ws [\s+] [ignored])
-____________
-…efbcd······
-      ^ [6 - 6] EOF Token(EOF)
+__________________
+····␣a␣ef␣␣b␣cd␣··
+      x [2] ef LiteralToken('ef')
+__________________
+···␣a␣ef␣␣b␣cd␣···
+      ^^ [3 - 4] ef LiteralToken('ef')
+__________________
+·␣a␣ef␣␣b␣cd␣·····
+      x [5] a LiteralToken('a')
+__________________
+·␣a␣ef␣␣b␣cd␣·····
+      ^^ [5 - 6] ws RegexToken(ws [\s+] [ignored])
+__________________
+…␣ef␣␣b␣cd␣·······
+      x [7] a LiteralToken('a')
+      x [7] ws RegexToken(ws [\s+] [ignored])
+__________________
+·␣a␣ef␣␣b␣cd␣·····
+      x [5] b LiteralToken('b')
+__________________
+…␣ef␣␣b␣cd␣·······
+      ^ [7 - 7] b LiteralToken('b')
+__________________
+…ef␣␣b␣cd␣········
+      x [8] cd LiteralToken('cd')
+__________________
+…ef␣␣b␣cd␣········
+      ^ [8 - 8] ws RegexToken(ws [\s+] [ignored])
+__________________
+…f␣␣b␣cd␣·········
+      ^^ [9 - 10] cd LiteralToken('cd')
+__________________
+…␣b␣cd␣···········
+      x [11] a LiteralToken('a')
+__________________
+…␣b␣cd␣···········
+      ^ [11 - 11] ws RegexToken(ws [\s+] [ignored])
+__________________
+…b␣cd␣············
+      x [12] a LiteralToken('a')
+      x [12] ws RegexToken(ws [\s+] [ignored])
+__________________
+…␣b␣cd␣···········
+      x [11] b LiteralToken('b')
+__________________
+…b␣cd␣············
+      x [12] b LiteralToken('b')
+__________________
+…␣b␣cd␣···········
+      x [11] EOF Token(EOF)
+__________________
+…b␣cd␣············
+      ^ [12 - 12] EOF Token(EOF)
 """
             )
         }
