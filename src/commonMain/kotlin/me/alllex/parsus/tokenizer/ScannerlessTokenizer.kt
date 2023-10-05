22cb43ef2cff2bac4a4eff9ae73c8d23cf895d15
Alex Semin <alllexsm@gmail.com>
2023-10-05 22:02:41 +0200

    Scannerless: cache ignored token last matching result

--- src/commonMain/kotlin/me/alllex/parsus/tokenizer/ScannerlessTokenizer.kt
+++ src/commonMain/kotlin/me/alllex/parsus/tokenizer/ScannerlessTokenizer.kt
@@ -16,27 +16,54 @@ internal class ScannerlessTokenizer(
 
     private val ignoredTokens = tokens.filter { it.ignored }
 
+    private var cachedIgnoredFromIndex: Int = -1
+    private var cachedIgnoredTokenMatch: TokenMatch? = null
+
     override fun findContextFreeMatch(fromIndex: Int): TokenMatch? = null
 
     override fun findMatchOf(fromIndex: Int, targetToken: Token): TokenMatch? {
         var pos = fromIndex
         while (true) {
-            matchImpl(pos, targetToken)?.let { return it }
+            matchTarget(pos, targetToken)?.let { return it }
 
-            val preIgnorePos = pos
-            for (ignoredToken in ignoredTokens) {
-                val ignoredMatch = matchImpl(pos, ignoredToken)
-                if (ignoredMatch != null) {
-                    pos = ignoredMatch.offset + ignoredMatch.length
-                    break
+            val ignoredMatch = matchIgnored(pos)
+            @Suppress("LiftReturnOrAssignment")
+            if (ignoredMatch != null) {
+                val posAfterIgnored = ignoredMatch.offset + ignoredMatch.length
+                if (posAfterIgnored > pos) {
+                    pos = posAfterIgnored
+                    continue
+                } else {
+                    // An ignored token matched, but it did not advance the position.
+                    // This should not happen normally, but this is a safeguard.
+                    return null
                 }
-            }
-
-            if (preIgnorePos == pos) {
-                // No ignored tokens matched, so we can't find the target token
+            } else {
+                // No ignored tokens matched at the current position either,
+                // so it is a mismatch overall
                 return null
             }
         }
         // The loop will exit via a mismatch, because no tokens can match "after the end of input"
     }
+
+    private fun matchIgnored(fromIndex: Int): TokenMatch? {
+        if (fromIndex == cachedIgnoredFromIndex) {
+            return cachedIgnoredTokenMatch
+        }
+
+        var match: TokenMatch? = null
+        for (ignoredToken in ignoredTokens) {
+            match = matchImpl(fromIndex, ignoredToken)
+            if (match != null) {
+                break
+            }
+        }
+
+        cachedIgnoredFromIndex = fromIndex
+        cachedIgnoredTokenMatch = match
+        return match
+    }
+
+    private fun matchTarget(pos: Int, targetToken: Token) = matchImpl(pos, targetToken)
 }
