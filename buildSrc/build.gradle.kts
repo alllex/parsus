d1e0b3302f43392468d92425ab8e7abb9bd6068b
Alex Semin <alllexsm@gmail.com>
2023-10-07 23:02:09 +0200

    Upgrade dokka to 1.9.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,5 +8,5 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVer")
     implementation("org.jetbrains.kotlin:kotlin-allopen:$kotlinVer")
     implementation("org.jetbrains.kotlinx:kotlinx-benchmark-plugin:0.4.8")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.8.20")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.0")
 }
